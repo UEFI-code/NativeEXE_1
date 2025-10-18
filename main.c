ee8e8126c9117ed64e4c18a43f393707eb268490
UEFI-code <zhuoshizhang@hotmail.com>
2025-10-18 21:50:00 +0900

    Nice Try

--- main.c
+++ main.c
@@ -1,11 +1,7 @@
-#include <ntddk.h>
-
 void NOP_Toy(void);
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
+__int8 entry()
 {
-	UNREFERENCED_PARAMETER(DriverObject);
-    UNREFERENCED_PARAMETER(RegistryPath);
 	while (1)
 	{
 		NOP_Toy();
