7b25a9beea627d9b12bec014effe08a06d63ecf4
UEFI-code <zhuoshizhang@hotmail.com>
2025-10-18 22:07:12 +0900

    Simply Code

--- toolget.s
+++ toolget.s
@@ -1,12 +1,13 @@
 .code
 
-NOP_Toy PROC
+entry PROC
+    int 3
     dd 90909090h
     dd 90909090h
     dd 90909090h
     dd 90909090h
     dd 90909090h
-    ret
-NOP_Toy ENDP
+    jmp entry
+entry ENDP
 
 END
