aadcfda8b5ad0097cd413d901f49cb1c3ca4d8e8
UEFI-code <zhuoshizhang@hotmail.com>
2025-10-19 12:10:23 +0900

    Try NtDrawText

--- main.c
+++ main.c
@@ -2,13 +2,19 @@
 
 void NOP_Toy();
 NTSTATUS NtDisplayString(PUNICODE_STRING DisplayString);
+NTSTATUS NtDrawText(PUNICODE_STRING Text);
+NTSTATUS NtDelayExecution(BOOLEAN Alertable, PLARGE_INTEGER DelayInterval);
 
 void entry()
 {
 	NOP_Toy();
-	DbgPrint("Hello from NativeEXE!\n");
-	NtDisplayString((PUNICODE_STRING)L"Hello from NativeEXE!\n");
+	UNICODE_STRING Text = RTL_CONSTANT_STRING(L"Hello, world!");
+    NtDrawText(&Text);
+    LARGE_INTEGER Timeout = {.QuadPart = 1 * 1000 * -10000LL};
+    NtDelayExecution(FALSE, &Timeout);
 	return;
 }
 
-// link main.obj toolget.obj ntdll.lib /SUBSYSTEM:NATIVE /ENTRY:entry /machine:x64 /out:NativeEXE_1.exe
+// link main.obj toolget.obj ntdll.lib /SUBSYSTEM:NATIVE /machine:x64 /ENTRY:entry /out:NativeEXE_1.exe
+
+// ref https://medium.com/windows-os-internals/windows-native-api-programming-hello-world-8f256abe1c85
